b80ac6044cbedb4b4587df7692b6d73a8d9c57d9
zorenal <zorenzal@gmail.com>
2024-02-23 15:48:00 -0800

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,51 @@
-const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  mode: 'production',
-    entry: './client/index.js',
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'build.js'
-    },
-    module: {
-        rules: [
-          {
-            test: /.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env', '@babel/preset-react']
-              }
-            },
+  mode: 'development',
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'build.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
-          {
-            test: /.(css|scss)$/,
-            exclude: /node_modules/,
-            use: [{
-              loader:'style-loader'
-            }, 
-            {
-              loader:'css-loader'
-            },],
-          }
+        },
+      },
+      {
+        test: /.(css|scss)$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          'css-loader',
         ],
       },
-      plugins: [
-        new HTMLWebpackPlugin({
-            template: './client/index.html'
-        })
     ],
-      devServer: {
-        static: {
-          publicPath :'/build',
-          directory:  path.resolve(__dirname, 'build'),
-        },
-        open: true,
-        proxy: {
-          '/': 'http://localhost:3000',
-        },
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './client/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      publicPath: '/build',
+      directory: path.resolve(__dirname, 'build'),
+    },
+    open: true,
+    proxy: {
+      '/api': {
+        target: `http://localhost:${PORT}`,
+        pathRewrite: { '^/api': '' },
       },
-}
+    },
+  },
+};
